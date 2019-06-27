90bc6d6843b174c1d999bfd65d707b9087641790
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-06-27 08:28:36 -0400

    chore: initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,55 @@
-export const helloWorld = (): string => "hello world";
+import { Compiler, Plugin } from "webpack";
+
+export default class CompilerPlugin extends Plugin {
+    private options: Options;
+    private defaultListeners: StageListeners = {
+        buildEnd: () => {},
+        buildStart: () => {},
+        compileEnd: () => {},
+        compileStart: () => {},
+    };
+
+    public constructor(options: Options) {
+        super();
+        this.options = this.validate(options);
+    }
+
+    private validate(options: Options) {
+        const validOptions: Options = { ...options, listeners: {} };
+        for (const stage of Object.keys(this.defaultListeners) as Stage[]) {
+            const listener = options.listeners[stage];
+            if (typeof listener === "function") {
+                validOptions.listeners[stage] = listener;
+            } else {
+                // eslint-disable-next-line no-console
+                console.warn("Ignoring invalid stage listener:", {
+                    stage,
+                    listener,
+                });
+                validOptions.listeners[stage] = this.defaultListeners[stage];
+            }
+        }
+        return options;
+    }
+
+    private cleanup(listener: Listener) {
+        process.on("exit", () => {
+            listener();
+        });
+        process.on("uncaughtException", e => {
+            console.error(e.stack); // eslint-disable-line no-console
+            process.exit(1);
+        });
+        process.on("SIGINT", () => {
+            process.exit(2);
+        });
+    }
+
+    public apply = (compiler: Compiler): void => {
+        const { name, listeners } = this.options;
+        compiler.hooks.afterPlugins.tap(name, listeners.buildStart);
+        compiler.hooks.compilation.tap(name, listeners.compileStart);
+        compiler.hooks.done.tap(name, listeners.compileEnd);
+        this.cleanup(listeners.buildEnd);
+    };
+}
